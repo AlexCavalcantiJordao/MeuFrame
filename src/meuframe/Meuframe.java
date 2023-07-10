942ed4b2de27f3fbc8312c26812f7b146a446011
AlexCavalcantiJordao <alex37705732@gmail.com>
2023-07-10 13:47:20 -0300

    build/ build.xml manifest.mf nbproject/ src/ test/

---
+++ src/meuframe/Meuframe.java
@@ -0,0 +1,23 @@
+package meuframe;
+
+import javax.swing.JFrame;
+import javax.swing.JButton;
+
+public class Meuframe extends JFrame {
+
+    public Meuframe() {
+
+        super("Meu Frame");
+
+        JButton botao = new JButton("Clique");
+        // JFrame frame = new JFrame();
+        getContentPane().add(botao);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(300, 300);
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new Meuframe();
+    }
+}
